9eb60c4a00b075b428b87e4d5f4eb14cbb789a19
Nour-Agha99 <temoagha@gmail.com>
2023-09-12 11:30:33 +0300

    b

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,40 +1,36 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
-import "../globals.css";
-import TopBar from "@/components/shared/TopBar";
-import LeftBar from "@/components/shared/LeftBar";
-import RightBar from "@/components/shared/RightBar";
-import BottomBar from "@/components/shared/BottomBar";
+const TopBar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [top, setTop] = useState("0");
 
-export const metadata = {
-  title: "Threads",
-  description: "A Meta Threads Application",
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+        setTop("0");
+      } else {
+        setTop("-100px");
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`sticky_nav text-dark-2 md:flex hidden hover:text-dark-1 font-medium`} style={{ top: top }}>
+      <Image src="/nLogo.svg" alt="logo" width={36} height={36} />
+      <div>hello</div>
+    </nav>
+  );
 };
 
-const inter = Inter({ subsets: ["latin"] });
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <ClerkProvider>
-      {
-        <html lang="en">
-          <body className={`${inter.className}`}>
-            <TopBar />
-            <main className="flex">
-              <LeftBar />
-              <section className="main-container">
-                <div className="w-full max-w-4xl">{children}</div>
-              </section>
-              <RightBar />
-            </main>
-            <BottomBar />
-          </body>
-        </html>
-      }
-    </ClerkProvider>
-  );
-}
+export default TopBar;
